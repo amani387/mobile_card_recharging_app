4ce059d7eb76d49f1479c7cff44edb4e65e8b609
amani387 <negaamanuel387@gmail.com>
2023-11-27 15:30:51 +0300

    homepage done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import './screens/home.dart';
 void main() {
   runApp(MyApp());
 }
@@ -8,65 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController _firstInputController = TextEditingController();
-  TextEditingController _secondInputController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Two Input Boxes'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _firstInputController,
-              decoration: InputDecoration(
-                labelText: 'First Input',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: _secondInputController,
-              decoration: InputDecoration(
-                labelText: 'Second Input',
-                border: OutlineInputBorder(),
-              ),
-
-            ),
-
-            SizedBox(height: 16.0),
-
-            ElevatedButton(
-              onPressed: () {
-                // Access the input values using the controllers
-                String firstInput = _firstInputController.text;
-                String secondInput = _secondInputController.text;
-
-                // Do something with the input values, e.g., print them
-                print('First Input: $firstInput');
-                print('Second Input: $secondInput');
-              },
-              child: Text('Submit'),
-            ),
-          ],
-        ),
-      ),
+      home: HomeScreen()
     );
   }
 }
---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+Drawer buildMyDrawer(BuildContext context) {
+  return Drawer(
+    child: ListView(
+      padding: EdgeInsets.zero,
+      children: [
+        DrawerHeader(
+          decoration: BoxDecoration(
+            color: Colors.blue,
+          ),
+          child: Text('Drawer Header'),
+        ),
+        ListTile(
+          title: Text('Item 1'),
+          onTap: () {
+            // Handle item 1 tap
+          },
+        ),
+        ListTile(
+          title: Text('Item 2'),
+          onTap: () {
+            // Handle item 2 tap
+          },
+        ),
+        // Add more items as needed
+      ],
+    ),
+  );
+}
